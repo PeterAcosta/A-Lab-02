739ce94794a6f06ec35df3241b370632c518eefd
Peter <peteracosta@gmail.com>
2023-06-05 20:02:01 -0300

    2023 Modificado en Linux Mint

---
+++ docker/01-entrypoint-cmd-01 OS/entrypoint.01.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# el caracter '#' indica que es un comentario
+# el caracter '!' indica que el archivo es un ejecutable. Análogo al .exe o .bat en Windows
+# '/bin/bash': bash es un tipo de shell, hay otros con los que se puede trabajar en linux si no estas seguro
+# de la version con la que trabajas puedes usar /bin/sh, sh es un alias al shell de tu sistema
+
+figlet $THIS_IMAGE
+neofetch
+
+# ambos comandos mantienen al contenedor corriendo
+# tail -f /dev/null
+
+echo "running like a daemon 'sleep infinity' ...."
+sleep infinity
+
+# tail -f /dev/null
+# top
