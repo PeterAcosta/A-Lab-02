06d2c7b8f463554e3d1771a6b3f8cda6fb296f2e
Peter <peteracosta@gmail.com>
2026-09-07 16:39:24 -0300

    Modificado en Dilma-Ubuntu 26.04

--- 01-sandbox-nginx-php-grunt/www/index.php
+++ 01-sandbox-nginx-php-grunt/www/index.php
@@ -1,14 +1,14 @@
 <?php
 
 require '_functions.php';
-
-
-echo "xxx".createH1Header('01-sandbox-nginx-php-grunt');
-
-echo "<a href='/index.html'>< < < Volver</a><br>";
-
 $fecha = date('Y-m-d H:i:s');
-echo "$fecha <br><br>";
+
+
+echo createH1Header('01-sandbox-nginx-php-grunt');
+
+echo "<a href='/index.html'>< < < Volver</a>";
+echo "   $fecha: ";
+
 
 
 phpinfo();
