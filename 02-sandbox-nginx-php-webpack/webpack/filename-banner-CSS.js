815e5b1fc675c84b849990c02f7e42085f8b35fb
Peter <peteracosta@gmail.com>
2026-09-09 23:39:21 -0300

    Modificado en Dilma-Ubuntu 26.04

--- 02-sandbox-nginx-php-webpack/webpack/filename-banner-CSS.js
+++
@@ -1,8 +0,0 @@
-// webpack-loaders/filename-banner-CSS.js
-const path = require('path');
-
-module.exports = function (source) {
-    const filename = path.basename(this.resourcePath);
-    // el "!" es importante — lo explico abajo
-    return `/*! esto viene del archivo ${filename} */\n${source}`;
-};
--- 02-sandbox-nginx-php-webpack/webpack/webpack.config.js
+++ 02-sandbox-nginx-php-webpack/webpack/webpack.config.js
@@ -3,6 +3,8 @@ const glob = require('glob');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { minify } = require('html-minifier-terser');
 
 const isProd = process.env.NODE_ENV === 'production';
 const distPath = 'www';
@@ -18,43 +20,74 @@ console.log('CSS encontrados:', cssFiles);
 
 module.exports = {
 
-    // mode: isProd ? 'production' : 'development',
-    // watch: !isProd,
+	// mode: isProd ? 'production' : 'development',
+	// watch: !isProd,
 
 	// mode: "development"
-    mode: "production",
-    
+	mode: "production",
+
 	watch: true, // <-- Habilita el modo watcher continuo
 	cache: { type: 'filesystem' },
 
-	
-    entry: {
-        main: jsFiles,     // -> www/_main.js
-        styles: cssFiles,  // -> www/_main.css (vía MiniCssExtractPlugin)
-    },
-    output: {
-        path: path.resolve(__dirname, distPath),
-        filename: '_[name].js', // genera _main.js
-    },
-    plugins: [
-        new RemoveEmptyScriptsPlugin(), // evita que se genere un _styles.js vacío
-        new MiniCssExtractPlugin({
-            filename: '_main.css',
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    { loader: 'css-loader', options: { url: false } },
-					{ loader: path.resolve(__dirname, 'webpack/filename-banner-CSS.js') },
-                ],
-            },
-        ],
-    },
-    optimization: {
-        minimizer: ['...', new CssMinimizerPlugin()],
-    },
+
+	entry: {
+		main: jsFiles,     // -> www/_main.js
+		styles: cssFiles,  // -> www/_main.css (vía MiniCssExtractPlugin)
+	},
+	output: {
+		path: path.resolve(__dirname, distPath),
+		filename: '_[name].js', // genera _main.js
+	},
+
+
+
+	plugins: [
+		new RemoveEmptyScriptsPlugin(), // evita que se genere un _styles.js vacío
+		new MiniCssExtractPlugin({
+			filename: '_main.css',
+		}),
+
+		new CopyWebpackPlugin({
+			patterns: [
+				{
+					from: 'sources/HTML/*.html',
+					to: 'HTML/[name][ext]', // -> www/HTML/archivo.html
+					transform: {
+						transformer: async (content) => {
+							return await minify(content.toString(), {
+								collapseWhitespace: true,
+								removeComments: true,
+								minifyCSS: true,
+								minifyJS: true,
+								removeRedundantAttributes: true,
+								removeScriptTypeAttributes: true,
+								removeStyleLinkTypeAttributes: true,
+								useShortDoctype: true,
+							});
+						},
+					},
+				},
+			],
+		}),
+
+	],
+
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{ loader: 'css-loader', options: { url: false } },
+				],
+			},
+		],
+	},
+	optimization: {
+		minimizer: ['...', new CssMinimizerPlugin()],
+	},
+
+
+
+
 };
